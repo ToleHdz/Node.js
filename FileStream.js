7ce7c0f622a0f91dde864cdc1a3b1739ad23c532
Jose Alberto Tolentino Hdz <32423935+ToleHdz@users.noreply.github.com>
2018-02-17 16:42:20 -0600

    FileStream para Node.js

---
+++ FileStream.js
@@ -0,0 +1,19 @@
+var http = require('http')				//para el servidor
+var FileStream = require('fs')			//para leer archivos
+
+
+//si nosotros agregamos el FileStream como respuesta del servidor, cada vez que nosotros hacemos una peticion, la pagina html se volvera a cargar con o sin cambios
+var server = http.createServer
+(
+	(request, response) => 
+	{
+		FileStream.readFile('./html/MiPagina.html',
+		(error, data) => 					//data es un objeto el cual contiene nuestra pagina web
+		{
+			response.write(data)			//escribe la pagina web
+			response.end(). 				//evita que el navegador siga cargando
+		});
+	}
+);
+
+server.listen(3000)
