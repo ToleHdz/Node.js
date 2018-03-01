b004af21c99fb1d55f99bc831d0bb42fd9515c90
Jose Alberto Tolentino Hdz <32423935+ToleHdz@users.noreply.github.com>
2018-03-01 14:31:34 -0600

    Agregando metodos a JS

--- html/js/main.js
+++ html/js/main.js
@@ -3,3 +3,13 @@ function mensaje()
 	var texto = document.getElementById('nombre').value
 	alert(texto)
 }
+
+function Alerta()
+{
+	alert("Mi Mensaje es ahora")
+}
+
+function Cadena() 
+{
+	return 'Mi Momento a llegado'
+}
